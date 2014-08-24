146531ea0789313ddcf07c594092466af6775ed6
Joe Friedl <joe@joefriedl.net>
2014-08-24 00:33:57 -0400

    Make the thing do the thing

--- downsample.py
+++ downsample.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import base64
+import random
+from flask import Flask, jsonify, request
+from PIL import Image
+from StringIO import StringIO
 
 
 app = Flask('downsample')
@@ -6,4 +10,28 @@ app = Flask('downsample')
 
 @app.route('/')
 def index():
+    """Returns a 200, that's about it!!!!!!!"""
     return 'Wow!!!!!'
+
+
+@app.route('/service', methods=['POST'])
+def service():
+    """Returns a low quality version of an incoming image."""
+    data = request.json
+
+    # Get the image data from the data URL.
+    header, image_data = data['content']['data'].split(',')
+    source = StringIO(base64.b64decode(image_data))
+    image = Image.open(source)
+
+    # Save the image at between 10 and 30 quality.
+    dest = StringIO()
+    image.save(dest, format='jpeg', quality=random.randrange(10, 30))
+
+    # Base64 encode the image.
+    dest.seek(0)
+    encoded_data = base64.b64encode(dest.read())
+
+    return jsonify({'content': {
+        'data': 'data:image/jpeg;base64,' + encoded_data,
+    }})
---
+++ tests/tests.py
@@ -0,0 +1,78 @@
+import base64
+from flask import json
+from mock import patch
+from PIL import Image
+from StringIO import StringIO
+
+from downsample import app
+
+
+class TestApp(object):
+    """Tests for the Flask app."""
+    def setup_method(self, method):
+        self.client = app.test_client()
+
+        with open('tests/butt.jpg') as image:
+            image_data = base64.b64encode(image.read())
+
+        self.data = {
+            'content': {
+                'data': 'data:image/jpeg;base64,' + image_data,
+            }
+        }
+
+    def _post_image(self):
+        """POSTs an image to /service."""
+        return self.client.post(
+            '/service',
+            data=json.dumps(self.data),
+            content_type='application/json',
+        )
+
+    def test_get_index(self):
+        """Should return a 200."""
+        response = self.client.get('/')
+
+        assert response.status_code == 200
+
+    def test_post_service(self):
+        """A POST to the /service endpoint should return a 200."""
+        response = self._post_image()
+
+        assert response.status_code == 200
+
+    def test_service_returns_json(self):
+        """/service should return JSON data."""
+        response = self._post_image()
+
+        assert response.headers['Content-Type'] == 'application/json'
+
+    def test_service_returns_jpeg(self):
+        """/service should return a jpeg image."""
+        response = self._post_image()
+        data = json.loads(response.data)
+
+        assert data['content']['data'].startswith('data:image/jpeg;base64,')
+
+    def test_service_returns_image_data(self):
+        """/service should return real jpeg data!"""
+        response = self._post_image()
+        data = json.loads(response.data)
+        header, image_data = data['content']['data'].split(',')
+        image_file = StringIO(base64.b64decode(image_data))
+
+        image = Image.open(image_file)
+
+        assert image.format == 'JPEG'
+        image.close()
+
+    @patch('downsample.Image')
+    def test_saves_low_quality(self, image_class):
+        """The image should be saved at a low quality."""
+        image = image_class.open.return_value
+        self._post_image()
+
+        args, kwargs = image.save.call_args
+
+        assert kwargs['quality'] <= 30
+        assert kwargs['quality'] >= 10
